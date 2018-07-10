6a9e8c8eabfb98da570bea31e4dc838639467be1
lichao15 <lichao15@staff.weibo.com>
2018-07-10 22:05:19 +0800

    update for leetcode: 560-subarraySum

--- algorithm_learning/src/main/java/com/jacobs/basic/algorithm/leetcode/ProblemsMedium_09.java
+++ algorithm_learning/src/main/java/com/jacobs/basic/algorithm/leetcode/ProblemsMedium_09.java
@@ -236,4 +236,37 @@ public class ProblemsMedium_09 {
         }
         return false;
     }
+
+    //    Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+    //
+    //        Example 1:
+    //    Input:nums = [1,1,1], k = 2
+    //    Output: 2
+    //    Note:
+    //    The length of the array is in range [1, 20,000].
+    //    The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+    public int subarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int result = 0;
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            Integer target = map.get(sum - k);
+            if (target != null) {
+                result += target;
+            }
+            if (map.containsKey(sum)) {
+                map.put(sum, map.get(sum) + 1);
+            } else {
+                map.put(sum, 1);
+            }
+        }
+
+        return result;
+    }
 }

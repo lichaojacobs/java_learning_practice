c646675435d76484b35e1c90dd049baa0d114627
lichao15 <lichao15@staff.weibo.com>
2018-07-10 10:51:43 +0800

    update for leetcode

--- algorithm_learning/src/main/java/com/jacobs/basic/algorithm/leetcode/ProblemsMedium_09.java
+++ algorithm_learning/src/main/java/com/jacobs/basic/algorithm/leetcode/ProblemsMedium_09.java
@@ -200,4 +200,40 @@ public class ProblemsMedium_09 {
 
         return -1;
     }
+
+    //    Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous subarray of size at least 2 that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
+    //
+    //        Example 1:
+    //    Input: [23, 2, 4, 6, 7],  k=6
+    //    Output: True
+    //    Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+    //    Example 2:
+    //    Input: [23, 2, 6, 4, 7],  k=6
+    //    Output: True
+    //    Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+    // 还是利用s[i,j]= s[0,j]-s[0,i]的概念，这里用到了余数的思想，如果上一个s[i] 余数和下一个s [j]余数相同，
+    // 说明i到j 之间 至少加了一倍的k的值
+    public boolean checkSubarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int runningSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            runningSum += nums[i];
+            if (k != 0) {
+                runningSum %= k;
+            }
+            Integer prev = map.get(runningSum);
+            if (prev != null) {
+                if (i - prev > 1) {
+                    return true;
+                }
+            } else {
+                map.put(runningSum, i);
+            }
+        }
+        return false;
+    }
 }

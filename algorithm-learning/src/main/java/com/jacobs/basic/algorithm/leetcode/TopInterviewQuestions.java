14903767f20582b76d0b96b1c9eb6101576cede1
lichao15 <lichao15@staff.weibo.com>
2019-01-13 01:25:52 +0800

    leetcode:341. Flatten Nested List Iterator

--- algorithm-learning/src/main/java/com/jacobs/basic/algorithm/leetcode/TopInterviewQuestions.java
+++ algorithm-learning/src/main/java/com/jacobs/basic/algorithm/leetcode/TopInterviewQuestions.java
@@ -7,9 +7,11 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * @author lichao
@@ -882,6 +884,57 @@ public class TopInterviewQuestions {
                 curr.next = null;
                 levelPreNode = null;
             }
+        }
+    }
+
+
+    //341. Flatten Nested List Iterator
+    //Given a nested list of integers, implement an iterator to flatten it.
+    //Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+    // This is the interface that allows for creating nested lists.
+    // You should not implement it, or speculate about its implementation
+    public interface NestedInteger {
+
+        // @return true if this NestedInteger holds a single integer, rather than a nested list.
+        public boolean isInteger();
+
+        // @return the single integer that this NestedInteger holds, if it holds a single integer
+        // Return null if this NestedInteger holds a nested list
+        public Integer getInteger();
+
+        // @return the nested list that this NestedInteger holds, if it holds a nested list
+        // Return null if this NestedInteger holds a single integer
+        public List<NestedInteger> getList();
+    }
+
+    public class NestedIterator implements Iterator<Integer> {
+
+        private Stack<NestedInteger> integerStack = new Stack<>();
+
+        public NestedIterator(List<NestedInteger> nestedList) {
+            for (int i = nestedList.size() - 1; i >= 0; i--) {
+                integerStack.push(nestedList.get(i));
+            }
+        }
+
+        @Override
+        public Integer next() {
+            return integerStack.pop().getInteger();
+        }
+
+        @Override
+        public boolean hasNext() {
+            while (!integerStack.isEmpty()) {
+                NestedInteger curr = integerStack.peek();
+                if (curr.isInteger()) {
+                    return true;
+                }
+                integerStack.pop();
+                for (int i = curr.getList().size() - 1; i >= 0; i--) {
+                    integerStack.push(curr.getList().get(i));
+                }
+            }
+            return false;
         }
     }
 }

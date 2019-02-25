1ec35623dfaf44be4ccd1968dea95f649993f477
lichao15 <lichao15@staff.weibo.com>
2019-02-25 15:03:06 +0800

    部分二分查找

--- algorithm-learning/src/main/java/com/jacobs/basic/algorithm/Practice.java
+++ algorithm-learning/src/main/java/com/jacobs/basic/algorithm/Practice.java
@@ -39,7 +39,37 @@ public class Practice {
         //        System.out.println(MoreThanHalfNum_Solution(new int[]{1, 2, 3, 2, 2, 2, 5, 4, 2}));
         //        getTwoMinNumber(new int[]{2, 2, 3, 2, 1, 2, 5, 3, 2}).stream().forEach(System.out::println);
         Practice practice = new Practice();
-        practice.eatBreads(3);
+        //        practice.eatBreads(3);
+        practice.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 4);
+    }
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[low] > nums[mid]) {
+                if (nums[high] >= target && nums[mid] < target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                if (nums[low] <= target && nums[mid] > target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+
+        return -1;
     }
 
     public static void mergeSort(int[] arr, int left, int right) {

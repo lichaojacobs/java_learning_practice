2c3ea9955ff7a3a74b4e92754abf1cccb6bf584b
lichao <lichao@kuaishou.com>
2019-06-22 19:20:35 +0800

    update dependency

--- utils/src/test/java/com/jacobs/UtilTest.java
+++ utils/src/test/java/com/jacobs/UtilTest.java
@@ -1,11 +1,13 @@
 package com.jacobs;
 
-import com.jacobs.CommonHttpUtil.HttpResponse;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+
 import okhttp3.Credentials;
+
 import org.junit.Test;
 
 /**
@@ -13,30 +15,30 @@ import org.junit.Test;
  */
 public class UtilTest {
 
-  private CommonHttpUtil commonHttpUtil = CommonHttpUtil.builder().build();
-  private SimpleDateFormat simpleDateTime = new SimpleDateFormat("yyyy/MM/dd");
+    private com.jacobs.CommonHttpUtil commonHttpUtil = com.jacobs.CommonHttpUtil.builder().build();
+    private SimpleDateFormat simpleDateTime = new SimpleDateFormat("yyyy/MM/dd");
 
-  public static void main(String[] args) {
-    System.out.println(Credentials.basic("root","root"));
-  }
-
-  @Test
-  public void testGetMethod() {
-    try {
-      //System.out.println(commonHttpUtil.get("http://visual-be/data").send().tryString());
-      Future<HttpResponse> responseFuture = commonHttpUtil.get("http://visual-be/data")
-          .async();
-      HttpResponse response = responseFuture.get(1000, TimeUnit.MILLISECONDS);
-      response.setJson(true);
-      System.out.println(response.tryJson());
-
-    } catch (Exception e) {
-      e.printStackTrace();
+    public static void main(String[] args) {
+        System.out.println(Credentials.basic("root", "root"));
     }
-  }
 
-  @Test
-  public void regTest() {
-    System.out.println(simpleDateTime.format(Calendar.getInstance().getTime()));
-  }
+    @Test
+    public void testGetMethod() {
+        try {
+            //System.out.println(commonHttpUtil.get("http://visual-be/data").send().tryString());
+            Future<com.jacobs.CommonHttpUtil.HttpResponse> responseFuture = commonHttpUtil.get("http://visual-be/data")
+                    .async();
+            com.jacobs.CommonHttpUtil.HttpResponse response = responseFuture.get(1000, TimeUnit.MILLISECONDS);
+            response.setJson(true);
+            System.out.println(response.tryJson());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void regTest() {
+        System.out.println(simpleDateTime.format(Calendar.getInstance().getTime()));
+    }
 }

888cdba6bdb5711c814da1700f45cc483ae924d1
lichao <lichao@kuaishou.com>
2019-11-18 15:28:30 +0800

    bounded priority blocking queue

---
+++ basic-learning/src/main/java/com/jacobs/basic/multithread/BoundedPriorityBlockingQueue.java
@@ -0,0 +1,169 @@
+package com.jacobs.basic.multithread;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * A wrapped implement for bounded PriorityBlockingQueue
+ */
+public class BoundedPriorityBlockingQueue<E> extends PriorityBlockingQueue<E> {
+
+    private static final Log LOG = LogFactory.getLog(BoundedPriorityBlockingQueue.class);
+
+    private PriorityBlockingQueue<E> priorityBlockingQueue;
+    private Method removeLastElem;
+    private volatile int maxQueueSize;
+    private AtomicLong currentSize;
+    private final ReentrantLock lock;
+
+    public BoundedPriorityBlockingQueue(int maxQueueSize) {
+        this.maxQueueSize = maxQueueSize;
+        this.currentSize = new AtomicLong(0);
+        this.lock = new ReentrantLock();
+        this.priorityBlockingQueue = new PriorityBlockingQueue<>(maxQueueSize);
+        try {
+            this.removeLastElem = PriorityBlockingQueue.class.getDeclaredMethod("removeAt", int.class);
+            this.removeLastElem.setAccessible(true);
+        } catch (NoSuchMethodException e) {
+            LOG.error("BoundedPriorityBlockingQueue init error", e);
+        }
+    }
+
+    @Override
+    public boolean offer(final E e) {
+        try {
+            LOG.warn(String.format("offer begin: %s", e));
+            lock.lockInterruptibly();
+            if (currentSize.get() <= maxQueueSize) {
+                return increase(e);
+            }
+
+            final Comparable firstElem = (Comparable) priorityBlockingQueue.peek();
+            final Comparable toCompareElem = (Comparable) e;
+            if (firstElem == null) {
+                return increase(e);
+            }
+            // high value with low priority element, should reject
+            if (toCompareElem.compareTo(firstElem) > 0) {
+                return false;
+            }
+
+            // higher priority element, should remove lowest element
+            decrease();
+            LOG.warn(String.format("remove oldest element"));
+            increase(e);
+            return true;
+        } catch (InterruptedException ex) {
+            LOG.error("BoundedPriorityBlockingQueue offer interrupted", ex);
+            return false;
+        } catch (Exception ex) {
+            LOG.error("BoundedPriorityBlockingQueue offer got exception", ex);
+            return false;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private boolean increase(final E e) {
+        currentSize.incrementAndGet();
+        return priorityBlockingQueue.offer(e);
+    }
+
+    private void decrease() throws InvocationTargetException, IllegalAccessException {
+        currentSize.decrementAndGet();
+        this.removeLastElem.invoke(priorityBlockingQueue, priorityBlockingQueue.size() - 1);
+    }
+
+    /**
+     * store job into level db async, cause serialize/deserialize may be slow
+     */
+    private static class AsyncTask implements Runnable, Comparable<AsyncTask> {
+
+        private String fileInfo;
+        private AsyncTaskEnum taskType;
+        private int priority;
+
+        public AsyncTask(String fileInfo, int priority) {
+            this.fileInfo = fileInfo;
+            this.taskType = AsyncTaskEnum.STORE;
+            this.priority = priority;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(100000L);
+            } catch (InterruptedException e) {
+                LOG.error(e);
+            }
+        }
+
+        public int getPriority() {
+            return priority;
+        }
+
+        @Override
+        public String toString() {
+            return "AsyncTask{" +
+                    "fileInfo='" + fileInfo + '\'' +
+                    ", taskType=" + taskType +
+                    ", priority=" + priority +
+                    '}';
+        }
+
+        @Override
+        public int compareTo(AsyncTask asyncTask) {
+            if (getPriority() < asyncTask.getPriority()) {
+                return -1;
+            }
+            if (getPriority() > asyncTask.getPriority()) {
+                return 1;
+            }
+            return 0;
+        }
+    }
+
+    /**
+     * task priority: task priority from move thread is higher
+     */
+    enum AsyncTaskPriorityEnum {
+        MOVE(0),
+        AD_HOC(1);
+
+        private int priority;
+
+        AsyncTaskPriorityEnum(int priority) {
+            this.priority = priority;
+        }
+
+        public int getPriority() {
+            return priority;
+        }
+    }
+
+    enum AsyncTaskEnum {
+        DELETE,
+        STORE,
+    }
+
+    public static void main(String[] args) {
+        BoundedPriorityBlockingQueue<AsyncTask> blockingQueue = new
+                BoundedPriorityBlockingQueue<>(5);
+
+
+        blockingQueue.offer(new AsyncTask("testMove", AsyncTaskPriorityEnum.MOVE.priority));
+        for (int i = 1; i < 10; i++) {
+            LOG.info(blockingQueue.offer(new AsyncTask("test", i)));
+        }
+        blockingQueue.offer(new AsyncTask("testMove2", AsyncTaskPriorityEnum.MOVE.priority));
+    }
+}
+
+
+
